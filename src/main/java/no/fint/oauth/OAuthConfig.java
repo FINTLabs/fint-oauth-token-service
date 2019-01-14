f619f2c8995ba5cab05b41ea40db2cc562d17a10
Asgeir Nilsen <asgeir@fintlabs.no>
2019-01-14 10:27:33 +0100

    Create default RestTemplate if OAuth is disabled.

--- src/main/java/no/fint/oauth/OAuthConfig.java
+++ src/main/java/no/fint/oauth/OAuthConfig.java
@@ -2,24 +2,27 @@ package no.fint.oauth;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 
 @Slf4j
-@ConditionalOnProperty(name = OAuthTokenProps.ENABLE_OAUTH, havingValue = "true")
 @Configuration
 public class OAuthConfig {
 
     @Bean
+    @ConditionalOnProperty(name = OAuthTokenProps.ENABLE_OAUTH, havingValue = "true")
     public OAuthTokenProps props() {
         return new OAuthTokenProps();
     }
 
     @Bean
+    @ConditionalOnProperty(name = OAuthTokenProps.ENABLE_OAUTH, havingValue = "true")
     public OAuth2RestTemplate oauth2RestTemplate() {
         OAuthTokenProps props = props();
         ResourceOwnerPasswordResourceDetails resourceDetails = new ResourceOwnerPasswordResourceDetails();
@@ -34,13 +37,22 @@ public class OAuthConfig {
     }
 
     @Bean
+    @ConditionalOnProperty(name = OAuthTokenProps.ENABLE_OAUTH, havingValue = "true")
     public OAuthRestTemplateFactory oAuthRestTemplateFactory() {
         return new OAuthRestTemplateFactory();
     }
 
     @Bean
+    @ConditionalOnProperty(name = OAuthTokenProps.ENABLE_OAUTH, havingValue = "true")
     public TokenService tokenService() {
         return new TokenService();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = OAuthTokenProps.ENABLE_OAUTH, matchIfMissing = true, havingValue = "false")
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        log.debug("OAuth disabled, loading standard RestTemplate");
+        return builder.build();
     }
 
 }
