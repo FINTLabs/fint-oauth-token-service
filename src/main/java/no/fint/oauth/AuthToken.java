f7d78507210be8f41eae646840d9380173acf6b3
Hknots <hknots99@gmail.com>
2024-11-22 10:39:37 +0100

    set expiresIn to unixTimeStampInSeconds

--- src/main/java/no/fint/oauth/AuthToken.java
+++ src/main/java/no/fint/oauth/AuthToken.java
@@ -5,8 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record AuthToken(
         @JsonProperty("access_token") String accessToken,
         @JsonProperty("token_type") String tokenType,
-        @JsonProperty("expires_in") int expiresIn,
+        @JsonProperty("expires_in") long expiresIn,
         @JsonProperty("acr") String acr,
         @JsonProperty("scope") String scope
 ) {
+
+    public AuthToken(String accessToken, String tokenType, long expiresIn, String acr, String scope) {
+        this.accessToken = accessToken;
+        this.tokenType = tokenType;
+        this.expiresIn = System.currentTimeMillis() + (expiresIn * 1000);
+        this.acr = acr;
+        this.scope = scope;
+    }
+
 }
