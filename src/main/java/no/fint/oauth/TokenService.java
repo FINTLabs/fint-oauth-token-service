6dca17f198c2cf1e698747ddc2e1f2cbca16080f
Hknots <hknots99@gmail.com>
2025-01-08 09:42:53 +0100

    support WebClient and RestClient

--- src/main/java/no/fint/oauth/TokenService.java
+++ src/main/java/no/fint/oauth/TokenService.java
@@ -1,26 +1,40 @@
 package no.fint.oauth;
 
 import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static no.fint.oauth.TokenClient.BEARER_TOKEN_TEMPLATE;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 @ConditionalOnProperty(value = "fint.oauth.enabled", havingValue = "true")
 public class TokenService {
 
-    private static final String BEARER_TOKEN_TEMPLATE = "Bearer %s";
-    private final RestClient oauthRestClient;
     private final OAuthTokenProps props;
     private final TokenInstance tokenInstance;
+    private final TokenClient tokenClient;
+
+    @Autowired
+    public TokenService(@Qualifier("oauthRestClient") RestClient restClient, OAuthTokenProps props) {
+        this.props = props;
+        this.tokenClient = new TokenClient(restClient, props);
+        this.tokenInstance = new TokenInstance(tokenClient, props);
+    }
+
+    public TokenService(@Qualifier("oauthWebClient") WebClient webClient, OAuthTokenProps props) {
+        this.props = props;
+        this.tokenClient = new TokenClient(webClient, props);
+        this.tokenInstance = new TokenInstance(tokenClient, props);
+    }
 
     @PostConstruct
     public void init() {
@@ -32,13 +46,7 @@ public class TokenService {
     }
 
     private void refreshConnection(String requestUrl) {
-        ResponseEntity<Void> response = oauthRestClient.get()
-                .uri(requestUrl)
-                .headers(header -> header.add(
-                        HttpHeaders.AUTHORIZATION, BEARER_TOKEN_TEMPLATE.formatted(tokenInstance.getAccessToken())
-                ))
-                .retrieve()
-                .toBodilessEntity();
+        ResponseEntity<Void> response = tokenClient.refresh(requestUrl, tokenInstance.getAccessToken());
         if (response.getStatusCode() != HttpStatus.OK) {
             throw new IllegalStateException(String.format("Unable to get access token from %s. Status: %d", props.getRequestUrl(), response.getStatusCode().value()));
         }
@@ -46,9 +54,10 @@ public class TokenService {
 
     public String getAccessToken(String requestUrl) {
         if (tokenInstance.isNull() || tokenInstance.hasExpired()) {
+            log.info("Refreshing Token...");
             tokenInstance.refreshToken();
-            refreshConnection(requestUrl);
         }
+        log.info("Giving existing token!");
         return tokenInstance.getAccessToken();
     }
 
